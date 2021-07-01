6568d3fe8c3d9b2017c8eeb8c7bfc1ec6dbcd01f
Victor Cleber <victor.cleber@outlook.com>
2021-07-01 23:57:58 +1200

    loading transactions on table through JavaScript

--- wallet.js
+++ wallet.js
@@ -14,3 +14,77 @@ const Modal = {
             .classList.remove('active');
     }
 }
+
+const transactions = [{
+        id:1,
+        description:'Power',
+        amount: -500000,
+        date: '26/01/2021'
+    },
+    {
+        id: 2,
+        description:'Website',
+        amount: 1000000,
+        date: '26/01/2021'
+    },
+    {   
+        id: 3,
+        description:'Internet',
+        amount: -300000,
+        date: '26/01/2021'
+    },
+    {   
+        id: 4,
+        description:'Water',
+        amount: -300000,
+        date: '26/01/2021'
+    }
+
+]
+
+const Transaction = {
+    incomes(){
+        //sum incomes
+    },
+    expenses(){
+        //sum expenses        
+    },
+    total(){
+        //incomes - expenses
+    }
+}
+
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+//Show transactions in HTML
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index){
+    
+        const tr = document.createElement('tr')
+        tr.innerHTML= DOM.innerHTMLtransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLtransaction(transaction){
+        const CSSClass = transaction.amount > 0 ? "income":"expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSClass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remove transaction">
+        </td>
+    `
+    return html
+    }
+ 
+}
+
+//method from collections object
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
