3ea4220106727c48ea1716fb9dfee43592de8f0f
Victor Cleber <victor.cleber@outlook.com>
2021-07-02 10:44:13 +1200

    Formating currency

--- wallet.js
+++ wallet.js
@@ -1,90 +1,95 @@
 const Modal = {
-    open() {
-        //open a modal
-        //add a class active to the modal
-        document
-            .querySelector('.modal-overlay')
-            .classList.add('active');
-    },
-    close() {
-        //close a modal
-        //remove a class active to the modal
-        document
-            .querySelector('.modal-overlay')
-            .classList.remove('active');
-    }
-}
+  open() {
+    //open a modal
+    //add a class active to the modal
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    //close a modal
+    //remove a class active to the modal
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
 
-const transactions = [{
-        id:1,
-        description:'Power',
-        amount: -500000,
-        date: '26/01/2021'
-    },
-    {
-        id: 2,
-        description:'Website',
-        amount: 1000000,
-        date: '26/01/2021'
-    },
-    {   
-        id: 3,
-        description:'Internet',
-        amount: -300000,
-        date: '26/01/2021'
-    },
-    {   
-        id: 4,
-        description:'Water',
-        amount: -300000,
-        date: '26/01/2021'
-    }
-
-]
+const transactions = [
+  {
+    id: 1,
+    description: "Power",
+    amount: -50000,
+    date: "26/01/2021",
+  },
+  {
+    id: 2,
+    description: "Website",
+    amount: 1000000,
+    date: "26/01/2021",
+  },
+  {
+    id: 3,
+    description: "Internet",
+    amount: -5489000,
+    date: "26/01/2021",
+  },
+  {
+    id: 4,
+    description: "Water",
+    amount: -300900,
+    date: "26/01/2021",
+  },
+];
 
 const Transaction = {
-    incomes(){
-        //sum incomes
-    },
-    expenses(){
-        //sum expenses        
-    },
-    total(){
-        //incomes - expenses
-    }
-}
+  incomes() {
+    //sum incomes
+  },
+  expenses() {
+    //sum expenses
+  },
+  total() {
+    //incomes - expenses
+  },
+};
 
 const Utils = {
-    formatCurrency(value){
-        const signal = Number(value) < 0 ? "-" : ""
-    }
-}
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("en-NZ", {
+      style: "currency",
+      currency: "NZD",
+    });
+
+    return signal + value;
+  },
+};
 //Show transactions in HTML
 const DOM = {
-    transactionsContainer: document.querySelector('#data-table tbody'),
-    addTransaction(transaction, index){
-    
-        const tr = document.createElement('tr')
-        tr.innerHTML= DOM.innerHTMLtransaction(transaction)
-        DOM.transactionsContainer.appendChild(tr)
-    },
-    innerHTMLtransaction(transaction){
-        const CSSClass = transaction.amount > 0 ? "income":"expense"
-        const amount = Utils.formatCurrency(transaction.amount)
-        const html = `
+  transactionsContainer: document.querySelector("#data-table tbody"),
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLtransaction(transaction);
+    DOM.transactionsContainer.appendChild(tr);
+  },
+  innerHTMLtransaction(transaction) {
+    const CSSClass = transaction.amount > 0 ? "income" : "expense";
+    const amount = Utils.formatCurrency(transaction.amount);
+    const html = `
         <td class="description">${transaction.description}</td>
-        <td class="${CSSClass}">${transaction.amount}</td>
+        <td class="${CSSClass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
             <img src="./assets/minus.svg" alt="Remove transaction">
         </td>
-    `
-    return html
-    }
- 
-}
+    `;
+    return html;
+  },
+};
 
 //method from collections object
-transactions.forEach(function(transaction){
-    DOM.addTransaction(transaction)
-})
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction);
+});
